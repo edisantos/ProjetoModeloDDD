913c36ebe8b2f6384e59982b0e0865acf83f9f4c
edisantos <edinaldolemos@yahoo.com.br>
2019-04-03 22:29:10 -0300

    Commit para implementação da camadade de Application

---
+++ ProjetoModeloDDD.Application/Interface/AppServiceBase.cs
@@ -0,0 +1,44 @@
+﻿using ProjetoModeloDDD.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoModeloDDD.Application.Interface
+{
+    public class AppServiceBase<TEntity> : IDisposable, IAppServiceBase<TEntity> where TEntity : class
+    {
+        private readonly ServiceBase<TEntity> _serviceBase;
+
+        public void Add(TEntity obj)
+        {
+            _serviceBase.Add(obj);
+        }
+
+        public void Dispose()
+        {
+            _serviceBase.Dispose();
+        }
+
+        public IEnumerable<TEntity> GetALL()
+        {
+            return _serviceBase.GetAll();
+        }
+
+        public TEntity GetById(int id)
+        {
+            return _serviceBase.GetById(id);
+        }
+
+        public void Remove(TEntity obj)
+        {
+            _serviceBase.Remove(obj);
+        }
+
+        public void Update(TEntity obj)
+        {
+            _serviceBase.Uppdate(obj);
+        }
+    }
+}
---
+++ ProjetoModeloDDD.Application/Interface/ClienteAppService.cs
@@ -0,0 +1,24 @@
+﻿using ProjetoModeloDDD.Domain.Entities;
+using ProjetoModeloDDD.Domain.Interfaces.Services;
+using System.Collections.Generic;
+
+namespace ProjetoModeloDDD.Application.Interface
+{
+
+
+    public class ClienteAppService : AppServiceBase<Cliente>, IClienteAppService
+    {
+        private readonly IClienteService _clienteService;
+
+        public ClienteAppService(IClienteService clienteService)
+         :base(clienteService)
+        {
+            _clienteService = clienteService;
+        }
+
+        public IEnumerable<Cliente> ObterClienteEspeciais()
+        {
+            return _clienteService.ObterClientesEspeciais(_clienteService.GetAll());
+        }
+    }
+}
